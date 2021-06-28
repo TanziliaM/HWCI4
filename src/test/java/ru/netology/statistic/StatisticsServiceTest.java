fd7b0733714ebe3e014a6b4eb7794cb771f165bc
TanziliaM <musatova.tanzilia@gmail.com>
2021-06-29 02:38:38 +0500

    commit 100%

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,61 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMin() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 3;
+
+        long actual = service.findMin(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findSum() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 85;
+
+        long actual = service.calculateSum(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findAverage() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 7;
+
+        long actual = service.calculateAverage(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findNumOfMonthBelowAverage() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 5;
+
+        long actual = service.calculateBelowAverage(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+}
