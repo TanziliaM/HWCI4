1484028f16d4a5fb3c6a79526d58c4493a82bf5d
unknown <musatova.tanzilia@gmail.com>
2021-08-01 12:38:56 +0300

    New test

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -16,5 +16,3 @@ public class StatisticsService {
     return current_max;
   }
 }
-
-
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StatisticsServiceTest {
+public class StatisticsServiceTest {
 
     @Test
     void findMax() {
@@ -17,5 +17,17 @@ class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    void findMinInMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {0, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+
 }
+
 
